a111ddc42350fe7686125ae4fbce9b6b97e6020e
seonyeongyoon <ssunnyy0204@gmail.com>
2024-03-29 00:38:51 +0900

    work 페이지 스크롤시 각 item들 fixed 되도록 수정

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,22 +1,30 @@
 import "@/styles/Work.scss";
-import { useEffect, useRef, useState } from 'react';
 import {
-  motion,
-  Variants
+  Variants,
+  motion
 } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
 
 
 const Work = () => {
-  const [isAnchorFixed, setIsAnchorFixed] = useState(false);
-  const anchorRef = useRef<HTMLDivElement | null>(null);
-
+  const [isFixedAnchors, setIsFixedAnchors] = useState(false);
+  const [isFixedIndexes, setIsFixedIndexes] = useState<number[]>([]);
+  const anchorRef = useRef<HTMLDivElement>(null);
+  const projectRefs = useRef<HTMLDivElement[]>([]);
   useEffect(() => {
+
     const handleScroll = () => {
-      if (window.scrollY > anchorRef.current!.offsetTop) {
-        setIsAnchorFixed(true);
-      } else {
-        setIsAnchorFixed(false);
-      }
+      const scrollTop = window.scrollY;
+
+      // 고정된 요소의 인덱스를 모아서 상태 업데이트
+      const fixedIndexes = projectRefs.current
+        .map((ref, index) => ref && scrollTop >= ref.offsetTop ? index : -1)
+        .filter(index => index !== -1);
+      
+      setIsFixedIndexes(fixedIndexes);
+
+      // 앵커가 상단에 닿으면 상태 업데이트
+      setIsFixedAnchors(window.scrollY > anchorRef.current!.offsetTop);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -25,8 +33,8 @@ const Work = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-
-
+  
+  
   const scrollToRef = (ref: React.MutableRefObject<HTMLDivElement | null>) => {
     if (ref.current) {
       window.scrollTo({
@@ -36,86 +44,71 @@ const Work = () => {
     }
   };
 
-  
-  const projectRefs = {
-    seah: useRef<null>(null),
-    seahAffiliates: useRef<null>(null),
-    bgf: useRef<null>(null),
-    cafeJIJO: useRef<null>(null),
-    dearXmas: useRef<null>(null)
-  };
-
-  const handleAnchorClick = (ref: React.MutableRefObject<HTMLDivElement | null>) => {
-    scrollToRef(ref);
+  const handleAnchorClick = (index: number) => {
+    if (projectRefs.current[index]) {
+      scrollToRef(projectRefs.current[index]);
+    }
   };
 
   
   const scrollVariants: Variants = {
     offscreen: {
-      y: `50vh`,
     },
     onscreen: {
       y: 0,
-      transition: {
-        //type: "spring",
-        //bounce: 0.4,
-        //duration: 0.8
-      }
     }
   };
 
-  const textVariants: Variants = {
-    offscreen: {
-      x: `100%`,
-    },
-    onscreen: {
-      x: 0,
-      transition: {
-        //type: "spring",
-        //bounce: 0.4,
-        duration: 1.2
-      }
-    }
-  }
 
+  const ParallaxText = ({ children }: { children: React.ReactNode }) => {
 
+    return (
+      <motion.div 
+        className="top"
+      >
+        {/* 여기에는 Parallax 효과를 적용할 컨텐츠를 넣습니다. */}
+        <h2>{children}&nbsp;</h2>
+        <h2>{children}&nbsp;</h2>
+        <h2>{children}&nbsp;</h2>
+        <h2>{children}&nbsp;</h2>
+      </motion.div>
+    );
+  };
+  
 
   return (
     
     <>
       <section className="intro">
         <h2 className="gallient">PROJECTS</h2>
-        <p>제가 작업한 project들을 소개합니다.</p>
+        <p>제가 작업한 project들을 소개합니다.😀</p>
       </section>
 
       <section className="works">
-        <div ref={anchorRef} className={`anchors ${isAnchorFixed ? 'fixed' : ''}`}>
+        <div ref={anchorRef} className={`anchors ${isFixedAnchors ? 'fixed' : ''}`}>
           <ul className="anchors__container">
-            <li data-index="1" onClick={() => handleAnchorClick(projectRefs.seah)}><h4>01&nbsp;SEAH</h4></li>
-            <li data-index="2" onClick={() => handleAnchorClick(projectRefs.seahAffiliates)}><h4>02&nbsp;SEAH AFFILATES</h4></li>
-            <li data-index="3" onClick={() => handleAnchorClick(projectRefs.bgf)}><h4>03&nbsp;BGF</h4></li>
-            <li data-index="4" onClick={() => handleAnchorClick(projectRefs.cafeJIJO)}><h4>04&nbsp;cafe JIJO</h4></li>
-            <li data-index="5" onClick={() => handleAnchorClick(projectRefs.dearXmas)}><h4>05&nbsp;Dear, Xmas</h4></li>
+            <li onClick={() => handleAnchorClick(0)}><h4>01&nbsp;SEAH</h4></li>
+            <li onClick={() => handleAnchorClick(1)}><h4>02&nbsp;SEAH AFFILATES</h4></li>
+            <li onClick={() => handleAnchorClick(2)}><h4>03&nbsp;BGF</h4></li>
+            <li onClick={() => handleAnchorClick(3)}><h4>04&nbsp;cafe JIJO</h4></li>
+            <li onClick={() => handleAnchorClick(4)}><h4>05&nbsp;Dear, Xmas</h4></li>
           </ul>
         </div>
 
         <div className="projects">
           {/* 1 */}
-          <motion.div 
-            className="projects__Item" 
+          <motion.div
+            className={`projects__Item ${isFixedIndexes.includes(0) ? 'fixed' : ''}`}
             initial="offscreen"
-            whileInView="onscreen" 
-            variants={scrollVariants}
-            ref={projectRefs.seah}
+            whileInView="onscreen"
+            ref={el => projectRefs.current[0] = el}
           >
-            <a href="https://www.seah.co.kr/" target="_blank">
+            <a 
+              href="https://www.seah.co.kr/" 
+              target="_blank" 
+            >
               <div className="imgContent">
-                <motion.div className="top" variants={textVariants}>
-                  <h2>SEAH&nbsp;</h2>
-                  <h2>SEAH&nbsp;</h2>
-                  <h2>SEAH&nbsp;</h2>
-                  <h2>SEAH&nbsp;</h2>
-                </motion.div>
+                <ParallaxText>SEAH GROUP</ParallaxText>
                 <div className="bottom">
                   <h3>01</h3><h3>SEAH GROUP</h3>
                 </div>
@@ -124,12 +117,7 @@ const Work = () => {
                 </figure>
               </div>
               <div className="textContent">
-                <motion.div className="top" variants={textVariants}>
-                  <h2>SEAH&nbsp;</h2>
-                  <h2>SEAH&nbsp;</h2>
-                  <h2>SEAH&nbsp;</h2>
-                  <h2>SEAH&nbsp;</h2>
-                </motion.div>
+              <ParallaxText>SEAH GROUP</ParallaxText>
                 <div className="bottom">
                   <p>정보 접근 취약자의 정보 격차를 해소하고, 홈페이지의 사용성을 증대시키기 위해<br/> 웹표준 및 웹접근성을 기반으로 설계하였습니다. 또한 어떤 환경에서도 동일한 경험을 전달하기 위해 풀 반응형 홈페이지로 구축했습니다. 나아가 글로벌 기업의 면모에 맞게 <span className="wsnr">해외 접속자도</span> 원활하게 정보를 볼 수 있도록 글로벌 서버 환경을 구성했습니다.</p>
                 </div>
@@ -138,20 +126,18 @@ const Work = () => {
           </motion.div>
 
           {/* 2 */}
-          <motion.div 
-            className="projects__Item" 
+          <motion.div
+            className={`projects__Item ${isFixedIndexes.includes(1) ? 'fixed' : ''}`}
             initial="offscreen"
-            whileInView="onscreen" 
-            variants={scrollVariants}
-            ref={projectRefs.seahAffiliates}
+            whileInView="onscreen"
+            ref={el => projectRefs.current[1] = el}
           >
-            <a href="https://www.seahsteel.co.kr/" target="_blank">
+            <a 
+              href="https://www.seahsteel.co.kr/" 
+              target="_blank" 
+            >
               <div className="imgContent">
-                <motion.div className="top" variants={textVariants}>
-                  <h2>SEAH AFFLIATES&nbsp;</h2>
-                  <h2>SEAH AFFLIATES&nbsp;</h2>
-                  <h2>SEAH AFFLIATES&nbsp;</h2>
-                </motion.div>
+                <ParallaxText>SEAH AFFLIATES</ParallaxText>
                 <div className="bottom">
                   <h3>02</h3><h3>SEAH AFFLIATES</h3>
                 </div>
@@ -160,11 +146,7 @@ const Work = () => {
                 </figure>
               </div>
               <div className="textContent">
-                <motion.div className="top" variants={textVariants}>
-                  <h2>SEAH AFFLIATES&nbsp;</h2>
-                  <h2>SEAH AFFLIATES&nbsp;</h2>
-                  <h2>SEAH AFFLIATES&nbsp;</h2>
-                </motion.div>
+                <ParallaxText>SEAH AFFLIATES</ParallaxText>
                 <div className="bottom">
                   <p>통합 웹사이트 정체성에 맞는 시각적 / 기능적인 일관성을 유지하기 위해, 브랜드 아이덴티티 요소를 체계적으로 재사용하고 각 UI 구성 요소를 모듈화한 디자인 시스템을 구축하였습니다. 모든 요소들은 계열사 각각의 콘텐츠 목적에 맞게끔 조화롭고 유연하게 배치할 수 있도록 제작 되었습니다.</p>
                 </div>
@@ -173,22 +155,18 @@ const Work = () => {
           </motion.div>
 
           {/* 3 */}
-          <motion.div 
-            className="projects__Item" 
+          <motion.div
+            className={`projects__Item ${isFixedIndexes.includes(2) ? 'fixed' : ''}`}
             initial="offscreen"
-            whileInView="onscreen" 
-            variants={scrollVariants}
-            ref={projectRefs.bgf}
+            whileInView="onscreen"
+            ref={el => projectRefs.current[2] = el}
           >
-            <a href="https://www.bgf.co.kr/" target="_blank">
+            <a 
+              href="https://www.bgf.co.kr/" 
+              target="_blank"
+            >
               <div className="imgContent">
-                <motion.div className="top" variants={textVariants}>
-                  <h2>BGF&nbsp;</h2>
-                  <h2>BGF&nbsp;</h2>
-                  <h2>BGF&nbsp;</h2>
-                  <h2>BGF&nbsp;</h2>
-                  <h2>BGF&nbsp;</h2>
-                </motion.div>
+                <ParallaxText>BGF</ParallaxText>
                 <div className="bottom">
                   <h3>03</h3><h3>BGF</h3>
                 </div>
@@ -197,13 +175,7 @@ const Work = () => {
                 </figure>
               </div>
               <div className="textContent">
-                <motion.div className="top" variants={textVariants}>
-                  <h2>BGF&nbsp;</h2>
-                  <h2>BGF&nbsp;</h2>
-                  <h2>BGF&nbsp;</h2>
-                  <h2>BGF&nbsp;</h2>
-                  <h2>BGF&nbsp;</h2>
-                </motion.div>
+                <ParallaxText>BGF</ParallaxText>
                 <div className="bottom">
                   <p>기존의 BGF 지주사 사이트 리뉴얼 작업을 담당하였습니다.<br/> 메인페이지는 전면 리뉴얼하였으며, 같은 레이아웃인데 각기 다른 구조로 코딩되어 있던 서브페이지들의 구조를 통일하는 작업을 진행하였습니다.</p>
                 </div>
@@ -212,20 +184,18 @@ const Work = () => {
           </motion.div>
           
           {/* 4 */}
-          <motion.div 
-            className="projects__Item" 
+          <motion.div
+            className={`projects__Item ${isFixedIndexes.includes(3) ? 'fixed' : ''}`}
             initial="offscreen"
-            whileInView="onscreen" 
-            variants={scrollVariants}
-            ref={projectRefs.cafeJIJO}
+            whileInView="onscreen"
+            ref={el => projectRefs.current[3] = el}
           >
-            <a href="https://cafe-jijo.vercel.app/" target="_blank">
+            <a 
+              href="https://cafe-jijo.vercel.app/" 
+              target="_blank"
+            >
               <div className="imgContent">
-                <motion.div className="top" variants={textVariants}>
-                  <h2>cafe JIJO&nbsp;</h2>
-                  <h2>cafe JIJO&nbsp;</h2>
-                  <h2>cafe JIJO&nbsp;</h2>
-                </motion.div>
+                <ParallaxText>cafe jijo</ParallaxText>
                 <div className="bottom">
                   <h3>04</h3><h3>cafe jijo</h3>
                 </div>
@@ -234,11 +204,7 @@ const Work = () => {
                 </figure>
               </div>
               <div className="textContent">
-                <motion.div className="top" variants={textVariants}>
-                <h2>cafe JIJO&nbsp;</h2>
-                  <h2>cafe JIJO&nbsp;</h2>
-                  <h2>cafe JIJO&nbsp;</h2>
-                </motion.div>
+                <ParallaxText>cafe jijo</ParallaxText>
                 <div className="bottom">
                   <p>풀 페이지 레이아웃의 카페 사이트입니다. 포켓베이스 SDK를 통해 데이터를 렌더링하였습니다. 로그인 / 회원가입, 장바구니, 지도 api를 이용한 메장찾기 기능, 게시판 기능을 구현하였습니다.</p>
                 </div>
@@ -247,20 +213,18 @@ const Work = () => {
           </motion.div>
 
           {/* 5 */}
-          <motion.div 
-            className="projects__Item" 
+          <motion.div
+            className={`projects__Item ${isFixedIndexes.includes(4) ? 'fixed' : ''}`}
             initial="offscreen"
-            whileInView="onscreen" 
-            variants={scrollVariants}
-            ref={projectRefs.dearXmas}
+            whileInView="onscreen"
+            ref={el => projectRefs.current[4] = el}
           >
-            <a href="https://dear-xmas.vercel.app/" target="_blank">
+            <a 
+              href="https://dear-xmas.vercel.app/" 
+              target="_blank"
+            >
               <div className="imgContent">
-                <motion.div className="top" variants={textVariants}>
-                  <h2>Dear, Xmas&nbsp;</h2>
-                  <h2>Dear, Xmas&nbsp;</h2>
-                  <h2>Dear, Xmas&nbsp;</h2>
-                </motion.div>
+                <ParallaxText>Dear, Xmas</ParallaxText>
                 <div className="bottom">
                   <h3>04</h3><h3>Dear, Xmas</h3>
                 </div>
@@ -269,11 +233,7 @@ const Work = () => {
                 </figure>
               </div>
               <div className="textContent">
-                <motion.div className="top" variants={textVariants}>
-                  <h2>Dear, Xmas&nbsp;</h2>
-                  <h2>Dear, Xmas&nbsp;</h2>
-                  <h2>Dear, Xmas&nbsp;</h2>
-                </motion.div>
+                <ParallaxText>Dear, Xmas</ParallaxText>
                 <div className="bottom">
                   <p>Dear, Xmas는 크리스마스의 감성과 특별한 순간을 함께 쌓아가는 웹 서비스입니다. 크리스마스와 관련된 컨텐츠를 즐길 수 있습니다. 코드 충돌을 줄이고 브랜치 관리가 용이한 Git Flow 방식을 사용하여 기능 브랜치를 만들고 각자 작업 브랜치를 따로 생성하여 작업하였습니다.</p>
                 </div>
